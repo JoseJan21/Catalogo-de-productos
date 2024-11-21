140fb20219e16de890d6cbe6717777b402dcb553
JoseJan21 <josejan303@gmail.com>
2024-11-21 15:56:27 -0300

    header

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
-// Esta función se ejecutará cuando se haga clic en el menú hamburguesa.
 function toggleMenu() {
     const nav = document.querySelector('nav');
+    const menuHamburguesa = document.querySelector('.menu-hamburguesa');
+    const links = document.querySelectorAll('nav a');
+
     nav.classList.toggle('active'); // Alterna la clase 'active' que muestra u oculta el menú
+
+    if (nav.classList.contains('active')) {
+        // Si el menú está activo, mostrar los enlaces
+        links.forEach(link => link.style.display = 'block');
+    } else {
+        // Si el menú no está activo, ocultar los enlaces
+        links.forEach(link => link.style.display = 'none');
+    }
 }
